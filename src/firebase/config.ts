29cffbbedfa7318fe9be757c901cb5b26373a1b7
Rysie Destiny <seryse@live.be>
2026-01-21 03:47:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7203304140-1f188",
+  "appId": "1:277114933989:web:922f1769b324bde01d7a0c",
+  "apiKey": "AIzaSyCmiMZ0QBkheLeQvyx7qcckuD8YT1BnBQ0",
+  "authDomain": "studio-7203304140-1f188.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "277114933989"
+};
