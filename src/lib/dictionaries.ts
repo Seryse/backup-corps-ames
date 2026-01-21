ea1c438e5dc08005b604c87fdd9a3e7a91e7fc01
Rysie Destiny <seryse@live.be>
2026-01-21 04:08:40 +0000

    desolee mais non ca marche pas. question : le fait que je ne sois pas co

--- src/lib/dictionaries.ts
+++ src/lib/dictionaries.ts
@@ -1,5 +1,3 @@
-import 'server-only';
-
 const dictionaries: { [key: string]: () => Promise<any> } = {
   en: () => import('@/dictionaries/en.json').then((module) => module.default),
   fr: () => import('@/dictionaries/fr.json').then((module) => module.default),
